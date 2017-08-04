20a4ee059374d5e7947c0f708c349d86974322ae
Nathan Grasset <nathan.grasset@recast.ai>
2017-08-04 09:47:25 +0200

    fix (core) fix notUnderstood middleware next parameter

--- src/middlewares/notUnderstood.ts
+++ src/middlewares/notUnderstood.ts
@@ -1,7 +1,7 @@
 import { ChatbotMiddleware, ChatbotMiddlewareNext, IChatbotContext } from '../Chatbot';
 
-export const notUnderstood = (messages: string[3], next: ChatbotMiddlewareNext): ChatbotMiddleware => {
-  return async (ctx: IChatbotContext): Promise<any> => {
+export const notUnderstood = (messages: string[3]): ChatbotMiddleware => {
+  return async (ctx: IChatbotContext, next: ChatbotMiddlewareNext): Promise<any> => {
     if (ctx.config.mongo && ctx.config.mongo.enabled === true && ctx.session) {
       const count: number = Math.min(ctx.session.consecutiveNotUnderstand, 2);
       ctx.session.consecutiveNotUnderstand++;
