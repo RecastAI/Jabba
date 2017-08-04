34df555f472dd54eb4e6a7f8b2eea6ae6ac8d0ab
Nathan Grasset <nathan.grasset@recast.ai>
2017-08-04 09:36:05 +0200

    feat(core) notUnderstood middleware now calls next all the time

--- src/middlewares/notUnderstood.ts
+++ src/middlewares/notUnderstood.ts
@@ -1,13 +1,15 @@
 import { ChatbotMiddleware, ChatbotMiddlewareNext, IChatbotContext } from '../Chatbot';
 
-export const notUnderstood = (messages: string[3]): ChatbotMiddleware => {
+export const notUnderstood = (messages: string[3], next: ChatbotMiddlewareNext): ChatbotMiddleware => {
   return async (ctx: IChatbotContext): Promise<any> => {
     if (ctx.config.mongo && ctx.config.mongo.enabled === true && ctx.session) {
       const count: number = Math.min(ctx.session.consecutiveNotUnderstand, 2);
       ctx.session.consecutiveNotUnderstand++;
       await ctx.session.save();
-      return ctx.message.reply([{ type: 'text', content: messages[count] }]);
+      await ctx.message.reply([{ type: 'text', content: messages[count] }]);
+      return next();
     }
-    return ctx.message.reply([{ type: 'text', content: messages[0] }]);
+    await ctx.message.reply([{ type: 'text', content: messages[0] }]);
+    return next();
   };
 };
