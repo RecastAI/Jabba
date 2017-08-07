9cfbd0bd03f529ae2e0453b4741920a71a61ce30
Nathan Grasset <nathan.grasset@recast.ai>
2017-08-07 11:10:25 +0200

    feat(session) better handling of consecutive not understand

--- src/middlewares/notUnderstood.ts
+++ src/middlewares/notUnderstood.ts
@@ -4,7 +4,7 @@ export const notUnderstood = (messages: string[3]): ChatbotMiddleware => {
   return async (ctx: IChatbotContext, next: ChatbotMiddlewareNext): Promise<any> => {
     if (ctx.config.mongo && ctx.config.mongo.enabled === true && ctx.session) {
       ctx.session.consecutiveNotUnderstand = ctx.session._previousNotUnderstand + 1;
-      const count: number = Math.min(ctx.session.consecutiveNotUnderstand - 1, 2);
+      const count: number = Math.min(ctx.session._previousNotUnderstand, 2);
       await ctx.session.save();
       await ctx.message.reply([{ type: 'text', content: messages[count] }]);
       return next();
